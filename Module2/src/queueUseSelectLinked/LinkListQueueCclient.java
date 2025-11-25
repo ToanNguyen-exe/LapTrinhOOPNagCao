43375a32e1b565fb9ce9b55f2cd5cf2f913be6c5
NDToanaz <mcmap2k6@gmail.com>
2025-11-25 08:57:24 +0700

    upd

---
+++ Module2/src/queueUseSelectLinked/LinkListQueueCclient.java
@@ -0,0 +1,15 @@
+package queueUseSelectLinked;
+
+public class LinkListQueueCclient {
+    public static void main(String[] args) {
+        MylinkedListQueue queue= new MylinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(90);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        System.out.println("dequeued item is "+ queue.dequeue().key);
+    }
+}
